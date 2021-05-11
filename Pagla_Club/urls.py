902eed7d1e4d7ac48cd3b3813504e2d0f03e47c1
Team-Explorer171 <teamexplorer171@gmail.com>
2021-05-11 09:05:25 +0600

    Evana Perveen

--- Pagla_Club/urls.py
+++ Pagla_Club/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('ckeditor-uploader/', include('ckeditor_uploader.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
